ba8c205b1b31723837aca3b577b15ad98ec4e5cd
Payne_xy <payne_xy@163.com>
2019-07-02 22:50:59 +0800

    启动页

---
+++ app/src/main/java/com/perdev/pithy/memo/activities/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.perdev.pithy.memo.activities
+
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Project    PithyMemo-git
+ * Path       com.perdev.pithy.memo.activities
+ * Date       2019/07/02 - 22:41
+ * Author     Payne.
+ * About      类描述：
+ */
+abstract class BaseActivity : AppCompatActivity(){
+    var mHandler:Handler=Handler();
+}
--- app/src/main/java/com/perdev/pithy/memo/activities/SplashActivity.kt
+++ app/src/main/java/com/perdev/pithy/memo/activities/SplashActivity.kt
@@ -1,13 +1,22 @@
 package com.perdev.pithy.memo.activities
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import com.perdev.pithy.memo.R
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        delayJump()
+    }
+
+    private fun delayJump() {
+        mHandler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1300L)
     }
 }
